020cb6d2e383469494abcc6767e814903f9fbd00
p1r2s-seth <77240227+p1r2s-seth@users.noreply.github.com>
2023-04-30 03:23:22 +0530

    all the changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Mahadeva
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/Mahadeva-bootstrap-Mahadevatics-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
@@ -18,15 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  /**
+  
+ /**
    * Sticky header on scroll
    */
-  const selectHeader = document.querySelector('#header');
-  if (selectHeader) {
-    document.addEventListener('scroll', () => {
-      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
-    });
-  }
+const selectHeader = document.querySelector('#header');
+if (selectHeader) {
+  document.addEventListener('scroll', () => {
+    window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+  });
+}
 
   /**
    * Scroll top button
@@ -47,58 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Mobile nav toggle
    */
-  const mobileNavShow = document.querySelector('.mobile-nav-show');
-  const mobileNavHide = document.querySelector('.mobile-nav-hide');
-
-  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
-    el.addEventListener('click', function(event) {
-      event.preventDefault();
-      mobileNavToogle();
-    })
-  });
-
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavShow.classList.toggle('d-none');
-    mobileNavHide.classList.toggle('d-none');
-  }
-
-  /**
-   * Hide mobile nav on same-page/hash links
-   */
-  document.querySelectorAll('#navbar a').forEach(navbarlink => {
-
-    if (!navbarlink.hash) return;
-
-    let section = document.querySelector(navbarlink.hash);
-    if (!section) return;
-
-    navbarlink.addEventListener('click', () => {
-      if (document.querySelector('.mobile-nav-active')) {
-        mobileNavToogle();
-      }
-    });
-
-  });
-
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
-
-  navDropdowns.forEach(el => {
-    el.addEventListener('click', function(event) {
-      if (document.querySelector('.mobile-nav-active')) {
-        event.preventDefault();
-        this.classList.toggle('active');
-        this.nextElementSibling.classList.toggle('dropdown-active');
-
-        let dropDownIndicator = this.querySelector('.dropdown-indicator');
-        dropDownIndicator.classList.toggle('bi-chevron-up');
-        dropDownIndicator.classList.toggle('bi-chevron-down');
-      }
-    })
-  });
+  
 
   /**
    * Initiate pURE cOUNTER
@@ -213,13 +156,131 @@ window.onclick = function (event) {
 // });
 
 // hamburger-navbar
-$(document).ready(function(){
-	$(".hamburger").click(function(){
-		$(".hamul").show()
-	});
+// $(document).ready(function(){
+// 	$(".hamburger").click(function(){
+// 		$(".hamul").show()
+// 	});
+// });
+// $(document).ready(function(){
+// 	$(".cross").click(function(){
+// 		$(".hamul").hide()
+// 	});
+// });
+
+
+
+
+
+
+
+
+
+
+/**
+ * Navbar links active state on scroll
+ */
+let navbarlinks = document.querySelectorAll('#navbar .scrollto');
+
+function navbarlinksActive() {
+  navbarlinks.forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    let position = window.scrollY;
+    if (navbarlink.hash != '#header') position += 200;
+
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active');
+    } else {
+      navbarlink.classList.remove('active');
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive);
+document.addEventListener('scroll', navbarlinksActive);
+
+/**
+ * Function to scroll to an element with top ofset
+ */
+function scrollto(el) {
+  const selectHeader = document.querySelector('#header');
+  let offset = 0;
+
+  if (selectHeader.classList.contains('sticked')) {
+    offset = document.querySelector('#header.sticked').offsetHeight;
+  } else if (selectHeader.hasAttribute('data-scrollto-offset')) {
+    offset = selectHeader.offsetHeight - parseInt(selectHeader.getAttribute('data-scrollto-offset'));
+  }
+  window.scrollTo({
+    top: document.querySelector(el).offsetTop - offset,
+    behavior: 'smooth'
+  });
+}
+
+/**
+ * Fires the scrollto function on click to links .scrollto
+ */
+let selectScrollto = document.querySelectorAll('.scrollto');
+selectScrollto.forEach(el => el.addEventListener('click', function(event) {
+  if (document.querySelector(this.hash)) {
+    event.preventDefault();
+
+    let mobileNavActive = document.querySelector('.mobile-nav-active');
+    if (mobileNavActive) {
+      mobileNavActive.classList.remove('mobile-nav-active');
+
+      let navbarToggle = document.querySelector('.mobile-nav-toggle');
+      navbarToggle.classList.toggle('bi-list');
+      navbarToggle.classList.toggle('bi-x');
+    }
+    scrollto(this.hash);
+  }
+}));
+
+/**
+ * Scroll with ofset on page load with hash links in the url
+ */
+window.addEventListener('load', () => {
+  if (window.location.hash) {
+    if (document.querySelector(window.location.hash)) {
+      scrollto(window.location.hash);
+    }
+  }
 });
-$(document).ready(function(){
-	$(".cross").click(function(){
-		$(".hamul").hide()
-	});
+
+/**
+ * Mobile nav toggle
+ */
+const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
+if (mobileNavToogle) {
+  mobileNavToogle.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+
+    this.classList.toggle('bi-list');
+    this.classList.toggle('bi-x');
+  });
+}
+
+/**
+ * Toggle mobile nav dropdowns
+ */
+const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+navDropdowns.forEach(el => {
+  el.addEventListener('click', function(event) {
+    if (document.querySelector('.mobile-nav-active')) {
+      event.preventDefault();
+      this.classList.toggle('active');
+      this.nextElementSibling.classList.toggle('dropdown-active');
+
+      let dropDownIndicator = this.querySelector('.dropdown-indicator');
+      dropDownIndicator.classList.toggle('bi-chevron-up');
+      dropDownIndicator.classList.toggle('bi-chevron-down');
+    }
+  })
 });
