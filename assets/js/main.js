c0c5d0113d8189f5fcf5875a2834cebb2fbd9084
p1r2s-seth <77240227+p1r2s-seth@users.noreply.github.com>
2023-04-30 11:16:27 +0530

    mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  
+
  /**
    * Sticky header on scroll
    */
@@ -21,6 +21,96 @@ if (selectHeader) {
     window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
   });
 }
+
+/**
+ * Mobile nav toggle
+ */
+const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
+if (mobileNavToogle) {
+  mobileNavToogle.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+
+    this.classList.toggle('bi-list');
+    this.classList.toggle('bi-x');
+  });
+}
+
+/**
+ * Navbar links active state on scroll
+ */
+let navbarlinks = document.querySelectorAll('#navbar .scrollto');
+
+function navbarlinksActive() {
+  navbarlinks.forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    let position = window.scrollY;
+    if (navbarlink.hash != '#header') position += 200;
+
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active');
+    } else {
+      navbarlink.classList.remove('active');
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive);
+document.addEventListener('scroll', navbarlinksActive);
+
+/**
+ * Function to scroll to an element with top ofset
+ */
+function scrollto(el) {
+  const selectHeader = document.querySelector('#header');
+  let offset = 0;
+
+  if (selectHeader.classList.contains('sticked')) {
+    offset = document.querySelector('#header.sticked').offsetHeight;
+  } else if (selectHeader.hasAttribute('data-scrollto-offset')) {
+    offset = selectHeader.offsetHeight - parseInt(selectHeader.getAttribute('data-scrollto-offset'));
+  }
+  window.scrollTo({
+    top: document.querySelector(el).offsetTop - offset,
+    behavior: 'smooth'
+  });
+}
+
+/**
+ * Fires the scrollto function on click to links .scrollto
+ */
+let selectScrollto = document.querySelectorAll('.scrollto');
+selectScrollto.forEach(el => el.addEventListener('click', function(event) {
+  if (document.querySelector(this.hash)) {
+    event.preventDefault();
+
+    let mobileNavActive = document.querySelector('.mobile-nav-active');
+    if (mobileNavActive) {
+      mobileNavActive.classList.remove('mobile-nav-active');
+
+      let navbarToggle = document.querySelector('.mobile-nav-toggle');
+      navbarToggle.classList.toggle('bi-list');
+      navbarToggle.classList.toggle('bi-x');
+    }
+    scrollto(this.hash);
+  }
+}));
+
+/**
+ * Scroll with ofset on page load with hash links in the url
+ */
+window.addEventListener('load', () => {
+  if (window.location.hash) {
+    if (document.querySelector(window.location.hash)) {
+      scrollto(window.location.hash);
+    }
+  }
+});
 
   /**
    * Scroll top button
@@ -176,95 +266,11 @@ window.onclick = function (event) {
 
 
 
-/**
- * Navbar links active state on scroll
- */
-let navbarlinks = document.querySelectorAll('#navbar .scrollto');
 
-function navbarlinksActive() {
-  navbarlinks.forEach(navbarlink => {
 
-    if (!navbarlink.hash) return;
 
-    let section = document.querySelector(navbarlink.hash);
-    if (!section) return;
 
-    let position = window.scrollY;
-    if (navbarlink.hash != '#header') position += 200;
 
-    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-      navbarlink.classList.add('active');
-    } else {
-      navbarlink.classList.remove('active');
-    }
-  })
-}
-window.addEventListener('load', navbarlinksActive);
-document.addEventListener('scroll', navbarlinksActive);
-
-/**
- * Function to scroll to an element with top ofset
- */
-function scrollto(el) {
-  const selectHeader = document.querySelector('#header');
-  let offset = 0;
-
-  if (selectHeader.classList.contains('sticked')) {
-    offset = document.querySelector('#header.sticked').offsetHeight;
-  } else if (selectHeader.hasAttribute('data-scrollto-offset')) {
-    offset = selectHeader.offsetHeight - parseInt(selectHeader.getAttribute('data-scrollto-offset'));
-  }
-  window.scrollTo({
-    top: document.querySelector(el).offsetTop - offset,
-    behavior: 'smooth'
-  });
-}
-
-/**
- * Fires the scrollto function on click to links .scrollto
- */
-let selectScrollto = document.querySelectorAll('.scrollto');
-selectScrollto.forEach(el => el.addEventListener('click', function(event) {
-  if (document.querySelector(this.hash)) {
-    event.preventDefault();
-
-    let mobileNavActive = document.querySelector('.mobile-nav-active');
-    if (mobileNavActive) {
-      mobileNavActive.classList.remove('mobile-nav-active');
-
-      let navbarToggle = document.querySelector('.mobile-nav-toggle');
-      navbarToggle.classList.toggle('bi-list');
-      navbarToggle.classList.toggle('bi-x');
-    }
-    scrollto(this.hash);
-  }
-}));
-
-/**
- * Scroll with ofset on page load with hash links in the url
- */
-window.addEventListener('load', () => {
-  if (window.location.hash) {
-    if (document.querySelector(window.location.hash)) {
-      scrollto(window.location.hash);
-    }
-  }
-});
-
-/**
- * Mobile nav toggle
- */
-const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
-if (mobileNavToogle) {
-  mobileNavToogle.addEventListener('click', function(event) {
-    event.preventDefault();
-
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-
-    this.classList.toggle('bi-list');
-    this.classList.toggle('bi-x');
-  });
-}
 
 /**
  * Toggle mobile nav dropdowns
